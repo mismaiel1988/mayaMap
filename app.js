15552c3fe8815e22184f06e6effdb1e767c1bcd0
Maya Ismaiel <maya.ismaiel@gmail.com>
2016-06-02 14:20:42 -0700

    my updated commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
-// mongoose 4.3.x
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var path = require('path');
+var fs = require('fs');
  
- app.use(bodyParser.json())
+app.use(bodyParser.json())
 
 var Schema = mongoose.Schema
 
@@ -15,6 +16,7 @@ mongoose.connect(mongodbUri);
 var db = mongoose.connection;  
 
 PostSchema = new Schema({
+  userId: String,
   elev: Number,
   lat: Number,
   long: Number,
@@ -28,9 +30,10 @@ app.use('piedparker.herokuapp.com', express.static(__dirname + '/public'));
 
 app.post('/update', function(req, res){
   
-	console.log('body: ' + JSON.stringify(req.body));
+	
 	var mypost = new Post()
-    mypost.elev = req.body.elev
+  mypost.userId = req.body.userId;
+      mypost.elev = req.body.elev
 		mypost.lat = req.body.lat
 		mypost.long = req.body.long
 		mypost.address = req.body.address
@@ -40,16 +43,83 @@ app.post('/update', function(req, res){
 	    mypost.save()
 	  }
 	);
+    //console.log(mypost.id);
+
+  res.send(JSON.stringify({id:mypost.id}));
 
 });
 
+app.post('/delete', function(req, res){
+  var id = req.body.id;
+  console.log('id is: '+id);
+  Post.findByIdAndRemove(id).exec();
+  var deleted=  true;/*  var post = Post.find({id: id});
+  if(post.remove())
+    deleted = true;
+  else 
+    deleted = false;*/
+    res.send( JSON.stringify({success: deleted}));
+});
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+
+app.get('/newUser', function(req, res){
+  res.sendFile(path.join(__dirname+'/public/new.html'));
+});
+
+
+UserSchema = new Schema({
+  username: String,
+  password: String
+}, { collection : 'users' });
+
+var User = mongoose.model('User', UserSchema);
+
+app.post('/new', function(req, res){
+  var username1 = req.body.username
+  ,   password1 = req.body.password
+  
+    var newUser = new User()
+      newUser.username = username1
+      newUser.password = password1
+      newUser.save(
+      function(err){
+        newUser.save()
+      }
+    );
+
+  console.log("new account created");
+ 
+ res.json(newUser);
+});
+
+app.post('/login', function(req, res){
+  var username1 = req.body.username
+  ,   password1 = req.body.password
+  var userAccount = User.findOne({username: username1, password: password1}, function(err, data) {
+    if(err){
+      res.send(JSON.stringify({success: false}));
+    }
+    else {
+        res.send(JSON.stringify({success: true, userId: data.id}));
+    }
+  })
+});
+
+
 app.get("/records", function(req, res) {
-    Post.find(function(err, data) {
+
+       Post.find({userId : req.query.userId},function(err, data) {
         if (err) {
-            console.log(err);
+            console.log('error'+err);
             return res(err);
         } else {
-            console.log(data.toString());
+            //console.log('data: '+data.toString());
             return res.json(data);
         }
     });
@@ -63,3 +133,4 @@ app.get("/records", function(req, res) {
 // });
 
 app.listen(process.env.PORT || 3000);
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,44 @@
+
 function initMap() {
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 17
-});
 
-var infoWindow = new google.maps.InfoWindow({map: map});
+	var map = new google.maps.Map(document.getElementById('map'), { zoom: 17 });
 
+	var infoWindow = new google.maps.InfoWindow({map: map});
 
-if (navigator.geolocation) {
+	if (navigator.geolocation) {
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-    
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	    
+	    var pos = {
+	      lat: position.coords.latitude,
+	      lng: position.coords.longitude
+	    };
 
-    infoWindow.setPosition(pos);
+	    infoWindow.setPosition(pos);
 
-    infoWindow.setContent("You are here");
+	    infoWindow.setContent("You're here");
 
-    map.setCenter(pos);
-	
-	var elevator= new google.maps.ElevationService;
-	var thisElevation;
-	elevator.getElevationForLocations({
-	    'locations': [pos]
-	  }, function(results, status) {
-			thisElevation = results[0].elevation;
-			getPos(pos, map, thisElevation);
+	    map.setCenter(pos);
+		
+		var elevator= new google.maps.ElevationService;
+		var thisElevation;
+		elevator.getElevationForLocations({
+		    'locations': [pos]
+		  }, function(results, status) {
+				thisElevation = results[0].elevation;
+				getPos(pos, map, thisElevation);
+		  });
+	    
+	  }, function() {
+	    handleLocationError(true, infoWindow, map.getCenter());
 	  });
-    
-  
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
-  });
-	} else {
-	  // Browser doesn't support Geolocation
-	  handleLocationError(false, infoWindow, map.getCenter());
+		} else {
+		  // Browser doesn't support Geolocation
+		  handleLocationError(false, infoWindow, map.getCenter());
+	}
 }
-}
+
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.setPosition(pos);
@@ -75,17 +75,21 @@ function getPos(pos, map, thisElevation) {
 		        	"lat": lat,
 		        	"long": long,
 		        	"address": address,
-		        	"time": d
+		        	"time": d,
+		        	"userId" : $('.user').val()
 		       		 }
 		        console.log(location)
 					$.ajax({
 						type: 'POST',
 						data: JSON.stringify(location),
+						dataType: 'json',
 				        contentType: 'application/json',
                         url: '/update',						
-                        success: function(location) {
-                            console.log('success');
-                            console.log(JSON.stringify(location));
+                        success: function(data) {
+                                                        
+                            var count = $('.record').length;                        
+							var newRec = $("<div class=\"record\" data-id='"+data.id+"'></div>").html((count+1)+".<br>"+"lat: "+lat+"<br>"+"long: "+long+"<br>"+"addy: "+address+"<br>"+"time: "+d+"<br>"+"elevation: "+thisElevation+"\n\n\n" + '<button class="delete">X</button>');
+							$(".records").append(newRec)	
                         }
                     });
 		      
@@ -100,22 +104,107 @@ function getPos(pos, map, thisElevation) {
 
 		geocodeLatLng(myLat, myLong, geocoder, map, thisElevation);
 	})
+}
+
+function login() {
+	var username = $(".username").val()
+	,	password = $(".password").val()
+
+	var user = {
+		
+		"username": username, 
+		"password": password
+
+		}
+	
+	$.ajax({
+		type: 'POST',
+		data: JSON.stringify(user),
+        contentType: 'application/json',
+        dataType: 'json',
+        url: '/login',
+        success: function(data) {
+        	if( !data.success ){
+        		alert("Failed to register"); return;
+        	}
+        	var userId = data.userId;
+        	$('.user').val(userId);   	//var user = data.user;
+        	localStorage.setItem("id", userId)
+        	/*localStorage.setItem("username", data[0].username)
+        	localStorage.setItem("loggedIn", true)*/
+        	$('.loginForm').remove();
+        	$('.mapContainer').removeClass('hidden');
+    		//$('.wrapper').html(data.content);
+    		initMap();
+    		$.get("/records?userId="+userId, function(data) {
+				for(var i = data.length - 1; i >= 0; i--){
+					var elev = data[i].elev;
+					var lat = data[i].lat;
+					var long = data[i].long;
+					var address = data[i].address;
+					var time = data[i].time;
+					var newRec = $("<div class=\"record\" data-id='"+data[i]._id+"'></div>").html(data.length - i +".<br>"+"lat: "+lat+"<br>"+"long: "+long+"<br>"+"addy: "+address+"<br>"+"time: "+time+"<br>"+"elevation: "+elev+"\n\n\n" + '<button class="delete">X</button>')
+					$(".records").append(newRec)
+				}
+			});
+        		
+        }
+    });
+	
+}
+
+function createAccount() {
+	window.location.href = "/new.html"
+}
+
+function createAndEnter() {
+
+	var username = $(".username").val()
+	,	password = $(".password").val()
+
+	var user = {
+		
+		"username": username, 
+		"password": password
+
+		}
+	
+	$.ajax({
+		type: 'POST',
+		data: JSON.stringify(user),
+        contentType: 'application/json',
+        dataType: 'json',
+        url: '/new',
+        success: function (data) {
+        	window.location.href = "index.html"
+        }
+    });
+
 
 }
 
-$.get( "/records", function(data) {
 
-	for(var i = data.length - 1; i >= 0; i--){
-		var elev = data[i].elev;
-		var lat = data[i].lat;
-		var long = data[i].long;
-		var address = data[i].address;
-		var time = data[i].time;
-		var newRec = $("<div class=\"record\"></div>").html(data.length - i +".<br>"+"lat: "+lat+"<br>"+"long: "+long+"<br>"+"addy: "+address+"<br>"+"time: "+time+"<br>"+"elevation: "+elev+"\n\n\n");
-		$(".records").append(newRec)
-	}
 
-  
-});
 
-initMap();
+$(document).on('click', '.delete', function(e){
+	e.preventDefault();
+	var parent = $(this).parent('.record');
+	$.ajax({
+		type: 'POST',
+		data: JSON.stringify({id:parent.data('id')}) ,
+        contentType: 'application/json',
+        dataType: 'json',
+        url: '/delete',
+        cache: false,					
+        success: function(data) {
+        	console.log('deleted response: ' + data.success);
+        	if(data.success) {
+            	parent.fadeOut().remove();
+            }
+        }
+    });
+})
+
+
+
+
